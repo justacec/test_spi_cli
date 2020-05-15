c0bbaae3a56db69641fbd5c06fa05ec8646469f8
Justace Clutter <justacec@gmail.com>
2020-05-15 14:21:35 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate json;
+
+use std::fmt::Debug;
+use std::vec::Vec;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use byte::ctx::*;
+use byte::{BytesExt, TryWrite, LE};
+
+
+fn main() {
+
+}
